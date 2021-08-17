30b9064ee35a71a6cb52c505f56b5b9698921296
Evgenii Moskalev(@moskalev27) <moskalev27@gmail.com>
2021-08-17 20:40:43 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0;
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let res = 0;
+    for (let i = 0; i < array.length; i++) {
+        res = (array[i] + res);
+        // console.log(res.toFixed(1));
+    }
+    return ((res / array.length).toFixed(2));
 }
+// console.log(avg([34,6,11,33,0,-10,10,3,22,-18,6,4,-21,32,23,18,-33,23,0,-1]));
